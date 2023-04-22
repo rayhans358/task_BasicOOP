1c5c22442f8dc4dd6111f456fa2a91e638aa1bd1
rayhans358 <rayhans358@gmail.com>
2023-04-22 23:05:17 +0700

    Eduwork_Tugas7

--- index.js
+++ index.js
@@ -1,47 +1,84 @@
-// import { Grid } from "gridjs";
-// import "gridjs/dist/theme/mermaid.css";
-// console.log(import);
+class Table {
+  constructor(datas, columns) {
+    this.datas = datas;
+    this.columns = columns;
+  };
+  
+  // Create Header
+  headRow() {
+    let open = `<thead><tr>`;
+    let close = `</tr></thead>`;
 
-new gridjs.Grid({
-  columns: ["Name", "Email", "Title"],
-  search: true,
-  sort: true,
-  pagination: {
-    limit: 5
+    this.columns.forEach((column) => {
+      open += `<th>${column}</th>`;
+    });
+    return open + close;
+  };
+
+  // Create Body
+  body() {
+    let open = `<tbody>`;
+    let close = `</tbody>`;
+
+    this.datas.forEach((row) => {
+      open += `<tr>`;
+
+      this.columns.forEach((column) => {
+        open += `<td>${row[column]}</td>`;
+      });
+      open += `</tr>`;
+    });
+    return open + close;
+  };
+
+  // Create Table
+  render(merger) {
+    let table =
+    `<table class="table table-hover" border="1">
+    ${this.headRow()}
+    ${this.body()}
+    </table>`;
+    merger.innerHTML = table;
+  };
+};
+
+// Input data and columns
+const data = [
+  {
+    Name: "Brenda E. Wilson",
+    Email: "nided42238@raotus.com",
+    Phone: "+1 505-346-4886",
   },
-  fixedHeader: true,
-  height: '400px',
-  data: [
-    ["Jonas", "nided42238@raotus.com", "+1 505-346-4886"],
-    ["Speed", "lihal43349@raotus.com", "+1 208-920-0638"],
-    ["Unohana", "kemabeh399@raotus.com", "+1 505-380-7325"],
-    ["Sarah", "woyic60144@raotus.com", "+1 332-823-9262"],
-    ["Keratos", "pameca7672@momoshe.com", "+1 219-991-0889"],
-    ["Kung Lao", "tipiyo9773@momoshe.com", "+81 70-9070-9418"],
-    ["Liu Kang", "waxam96086@momoshe.com", "+81 80-7458-3795"],
-    ["Benkai", "rixesy@brand-app.biz", "+90 561 615 20 75"],
-    ["Potter", "pozusa@lyft.live", "+90 507 123 24 50"],
-    ["Ashfarin", "lodyca@clout.wiki", "+598 91 485 871"],
-  ]
-}).render(document.getElementById("wrapper"));
+  {
+    Name: "Maria Luisa Padovesi",
+    Email: "pozusa@lyft.live",
+    Phone: "+39 375 984 463",
+  },
+  {
+    Name: "Unohana Retsu",
+    Email: "kemabeh399@raotus.com",
+    Phone: "+81 90-3374-8138",
+  },
+  {
+    Name: "Kung Lao",
+    Email: "tipiyo9773@momoshe.com",
+    Phone: "+90 561 615 20 75",
+  },
+  {
+    Name: "Liu Kang",
+    Email: "waxam96086@momoshe.com",
+    Phone: "+420 608 663 220",
+  },
+  {
+    Name: "Shang Tsung",
+    Email: "lodyca@clout.wiki",
+    Phone: "+86 174 0133 9258",
+  },
+];
 
-// const faker = require('faker');
-// import { faker } from '@faker-js/faker';
-// console.log(faker);
+const columns = ["Name", "Email", "Phone"];
 
-// const grid = new Grid({
-//   columns: [
-//       'Name',
-//       'Email',
-//       'Title',
-//    ],
-//   sort: true,
-//   pagination: true,
-//   fixedHeader: true,
-//   height: '400px',
-//   data: Array(50).fill().map(x => [
-//     faker.name.findName(),
-//     faker.internet.email(),
-//     faker.name.title(),
-//   ])
-// }).render(document.getElementById("wrapper"));
+const table = new Table(data, columns);
+
+const library = document.getElementById("library");
+table.render(library);
